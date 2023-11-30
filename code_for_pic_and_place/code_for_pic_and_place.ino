d37c0df74fa4ff6285eb99a7768a94cc41cff00a
Hashika Chathubhashaka <74332679+HashikaChathubhashaka@users.noreply.github.com>
2023-11-30 13:08:56 +0530

    Update code_for_pic_and_place.ino
    put code for pic and place (not tested)

--- code_for_pic_and_place/code_for_pic_and_place.ino
+++ code_for_pic_and_place/code_for_pic_and_place.ino
@@ -1,13 +1,62 @@
 #include <Servo.h>
-#include <NewPing.h>
+
+// Create servo objects.
+Servo servoA; // Base Servo: Rotate entire arm left and right.
+Servo servoB; // Arm Servo: Moves arm up and down.
+Servo servoC; // Arm Servo: Twist arm left and right.
+Servo servoD; // Claw Servo: Open and closes the claw.
+
+
+void resetArm()
+{
+  // Arm resting position.
+  servoA.write(0);
+  servoB.write(0);
+  servoC.write(0);
+  servoD.write(90);
+}
+
+void grabItem()
+{
+  servoD.write(180);
+  delay(1000);
+  servoA.write(angleA);
+  delay(1000);
+  servoC.write(angleC);
+  delay(1000);
+  servoD.write(110);
+  delay(1000);
+}
+
+void dropItem()
+{
+  servoB.write(180);
+  delay(1000);
+  servoD.write(180);
+  delay(1000);
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
-  
 
+  // Servos attached to corresponding PWM pins.
+  servoA.attach(9);
+  servoB.attach(6);
+  servoC.attach(5);
+  servoD.attach(3);
+
+  resetArm();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+  grabItem(); // Grabs item.
+
+  delay(1000);
+  dropItem(); // Drops item to left of arm.
+  delay(1000);
+  //resetArm(); // Brings arm back to resting position.
+  
 
 }
