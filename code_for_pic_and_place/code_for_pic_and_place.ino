3240dce98e4cd75d16e9b7a7d732cdf6ee68d3b3
Hashika Chathubhashaka <74332679+HashikaChathubhashaka@users.noreply.github.com>
2023-11-30 13:15:24 +0530

    Update code_for_pic_and_place.ino
    putting initial and final positions

--- code_for_pic_and_place/code_for_pic_and_place.ino
+++ code_for_pic_and_place/code_for_pic_and_place.ino
@@ -6,7 +6,19 @@ Servo servoB; // Arm Servo: Moves arm up and down.
 Servo servoC; // Arm Servo: Twist arm left and right.
 Servo servoD; // Claw Servo: Open and closes the claw.
 
+//initial position
+int angleA1 = 
+int angleB1 =
+int angleC1 =
+int angleD1 =
+  
+//final position
+int angleA2 = 
+int angleB2 =
+int angleC2 =
+int angleD2 =
 
+  
 void resetArm()
 {
   // Arm resting position.
@@ -18,6 +30,7 @@ void resetArm()
 
 void grabItem()
 {
+  //picking the object
   servoD.write(180);
   delay(1000);
   servoA.write(angleA);
@@ -30,6 +43,7 @@ void grabItem()
 
 void dropItem()
 {
+  //placing the object
   servoB.write(180);
   delay(1000);
   servoD.write(180);
@@ -44,7 +58,8 @@ void setup() {
   servoB.attach(6);
   servoC.attach(5);
   servoD.attach(3);
-
+  
+  //start with beging position.
   resetArm();
 }
 
